b0d8c7b889f8078911d62195a1f7e6a99173478b
felipe.fernandes <felipe.fernandes@meta.com.br>
2019-10-07 14:19:58 -0300

    2019/10/07 14:19

---
+++ exercicios-react/src/componentes/DoisComponentes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CompA = promps => (
+    <h1>Primeiro diz: {promps.valor}</h1>
+);
+
+const CompB = promps => (
+    <h1>Segundo diz: {promps.valor}</h1>
+);
+
+export { CompA, CompB };
+// export default CompA;
+
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeiroComponente from './componentes/PrimeiroComponente';
+// import PrimeiroComponente from './componentes/PrimeiroComponente';
+// import { CompA, CompB as B } from './componentes/DoisComponentes';
+// import MultiElementos from './componentes/MultiElementos';
+// import FamiliaSilva from './componentes/FamiliaSilvia';
+import Familia from './componentes/Familia';
+import Membro from './componentes/Membro';
 
 const elemento = document.getElementById('root');
 
 ReactDOM.render(
     <div>
-        <PrimeiroComponente valor="Bom dia!" />
+        <Familia sobrenome="Pereira">
+            <Membro nome="Andre" />
+            <Membro nome="Mariana" />
+        </Familia>
+        {/* <FamiliaSilva /> */}
+        {/* <MultiElementos /> */}
+        {/* <CompA valor="Olá eu sou A" />
+        <B valor="B na área" /> */}
+        {/*<PrimeiroComponente valor="Bom dia!" /> */}
     </div>
 , elemento);
 
